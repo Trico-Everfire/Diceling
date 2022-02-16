90a034ecfdc4fec4d54c9d7bc07ed39a81995c88
Trico-Everfire <bobbys@live.nl>
2022-02-16 21:59:43 +0100

    Fixed Config Missing.

---
+++ widgets/ChiraOpenGLWidget.cpp
@@ -0,0 +1,5 @@
+//
+// Created by trico on 16-2-22.
+//
+
+#include "ChiraOpenGLWidget.h"
---
+++ widgets/ChiraOpenGLWidget.h
@@ -0,0 +1,10 @@
+//
+// Created by trico on 16-2-22.
+//
+
+#include <QOpenGLWidget>
+
+class ChiraOpenGLWidget : public QOpenGLWidget
+{
+
+};
